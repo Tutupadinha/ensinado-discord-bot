f1b952d7c118e7b3a1be06c9e7af7f59e4b7876d
LordLuch <65835432+LordLuch@users.noreply.github.com>
2020-07-18 21:20:34 -0300

    Update ajuda.js

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -1,12 +1,6 @@
 const Discord = require("discord.js")
 exports.run = async (bot, message, argumentos, arg_texto, chat) => {
-  const ajuda = new Discord.MessageEmbed()
   
-    .setColor("GREEN")
-    .setTitle("Lista de comandos!")
-    .setDescription(`t.ajuda - mostra este comando \nt.say - Me faça falar algo \nt.ban - bana um membro \nt.unban - desbane um membro \nt.listban - mostra a lista de banidos`)
-    .setTimestamp()
-    .setFooter(`Comando solicitado por ${message.member.displayName}`, message.author.displayAvatarURL({Size: 32}))   
+  message.channel.send("Ainda não tenho comandos definidos!")
   
-  message.channel.send(ajuda)
 }
