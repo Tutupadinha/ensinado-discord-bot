868feb92e3d0398cdce1ac49d23dd1e3b3185c56
LordLuch <65835432+LordLuch@users.noreply.github.com>
2020-07-20 21:12:10 -0300

    Update ban.js

--- comandos/ban.js
+++ comandos/ban.js
@@ -1,44 +1,45 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-  let ban = new Discord.MessageEmbed()
-    .setColor("RED")
-    .setFooter(`Comando solicitado por ${message.member.displayName}`, message.author.displayAvatarURL({size: 32}))
-    .setTimestamp()
+
+let xdemb = new Discord.MessageEmbed()
+    .setColor("RANDOM")
     .setTitle("Comando de ban")
-    .addField("Descrição:", `Bane um membro`, true)
-    .addField("Uso:", `t.ban [usuário] [motivo]`, true)
-    .addField("Exemplo:", `t.ban @lordluch spam`)
-  
-  if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Você não tem permissão para fazer isso!");
-  
-  let member = message.mentions.members.first();
-    if(!member) return message.channel.send(ban)
-      if(!member.bannable) return message.channel.send("Eu não consigo banir este usuário")
-    if(member.user.id === "384799694569734145") return message.channel.send("Eu não consigo banir o dono!")
-      if(member.user.id === message.author.id) return message.channel.send("Você não pode banir a si mesmo")
-  
-  let motivo = args.slice(1).join(" ");
-    if(!motivo) {
-      return message.channel.send("Não especificou o motivo!")
+    .addField("Description:", `Bane um membro`, true)
+    .addField("Usage:", `${bot.prefixo}ban [usuário] [motivo]`, true)
+    .addField("Example:", `${bot.prefixo}ban @LordLuch spam`)
+
+    if(message.member.permissions.has("BAN_MEMBERS")) { 
+     if(!message.guild.me.permissions.has(["BAN_MEMBERS"])) return message.channel.send("Eu naõ tenho permissão para fazer isto.")
+      
+    let member = message.mentions.members.first();
+    if(!member) return message.channel.send(xdemb)
+    if(!member.bannable) return message.channel.send("Eu não consigo banir este usuário!")
+    if(member.user.id === "seu_id") return message.channel.send("Eu não consigo banir o dono!")
+
+    if(member.id === message.author.id) return message.channel.send("Você não pode banir a si mesmo!")
+
+  let reason = args.slice(1).join(" ");
+    if(!reason) {
+      return message.channel.send("Não especificou uma razão!");
     } else {
-      var mtv = motivo
+       var res = reason
     }
-  
-  await member.ban(motivo).catch(error => message.channel.send("Eu não consegui executar o comando por que "+error))
 
-  let banido = new Discord.MessageEmbed()
-    .setColor("GREEN")
-      .setFooter(`Comando solicitado por ${message.member.displayName}`, message.author.displayAvatarURL({size: 32}))
-        .setTitle(`Ban | ${member.user.tag}`)
-          .addField("Usuário", member, true)
-        .addField("Moderador", message.author, true)
-      .addField("Motivo", mtv)
-    .setTimestamp()
-  
-  message.channel.send(banido)
-  message.guild.channels.cache.get("725486321212194937").send(banido)
+    await member.ban(reason).catch(error => message.channel.send(`Desculpe eu não consegui por causa de: ${error}`));
 
-
-
-}
+    let staff = new Discord.MessageEmbed()
+      .setColor("#00FFFF")
+      .setTitle(":warning: | Ban")
+      .setDescription("O usuário: <@"+member.id+"> foi banido por <@"+message.author.id+"> Motivo\n``"+res+"``")
+      .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({Size: 32}))
+      .setTimestamp()
+      .setFooter("ID do usuário: "+message.author.id)
+    
+    message.channel.send("O Usuário <@"+member.id+"> foi banido! Esse bobão achou que podia quebrar as regras e estava muito enganado!")
+      
+    message.delete()
+    } else {
+      return message.channel.send("Você não tem permissões para fazer isso!");
+    }
+  }
