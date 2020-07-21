483cd2bcfc9a0ae0c93549f6522de56ca1616423
LordLuch <65835432+LordLuch@users.noreply.github.com>
2020-07-20 21:31:31 -0300

    Create messageDelete.js

---
+++ eventos/messageDelete.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+
+module.exports = async (bot, message) => { //cuida do evento de mensagens enviadas em chat 
+
+ let embed = new Discord.MessageEmbed()
+    .setColor("RED")
+      .setTitle("Mensagem deletada!")
+    .setDescription("O usuário <@"+message.author+"> deletou a mensagem "+message.content)  
+ bot.channels.cache.get("id_do_canal").send(embed)
+
+}
