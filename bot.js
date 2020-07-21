644ece40930702b7282eef272123269392adf15e
LordLuch <65835432+LordLuch@users.noreply.github.com>
2020-07-20 21:08:20 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ const fs = require("fs");
 bot.fs = fs;
 //bot.mathjs = require("mathjs");
 
-bot.prefixo = 't.';
+bot.prefixo = 'coloque o prefixo desejado aqui!';
 
 /*  Carrega os códigos dos módulos: comandos e sistemas  */
 bot.pastas = {}; //vai associar um comando a uma paste
