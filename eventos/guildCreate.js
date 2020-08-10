465966b0d8f3208d89b88b3b62c16c09303c588e
LordLuch <65835432+LordLuch@users.noreply.github.com>
2020-08-10 10:44:24 -0300

    Update guildCreate.js

--- eventos/guildCreate.js
+++ eventos/guildCreate.js
@@ -1,5 +1,21 @@
 module.exports = async (bot, guild) => { //faz algo qdo o bot liga
+  await bot.Database.Guilda.findOne({
+    "guild_id": guild.id
+  }, function (erro, documento) {
+    if(documento) {
+      documento.prefixo = "seu prefixo aqui"  
+      documento.save()
+    } else {
+      new bot.Databse.Guilda({
+        guild_id: guild.id,
+        prefixo: "seu prefixo aqui",
+      }).save()
+    }
+    if(erro) {
+      console.log(erro)
+    }
+  })
   
-  console.log("Eu fui adicionado na guilda: "+guild.name)
+console.log("Eu fui adicionado na guilda: "+guild.name)
 
 }
