5eb2f540e31a62041969c1ac00093f550a2efa14
LordLuch <65835432+LordLuch@users.noreply.github.com>
2020-08-10 10:43:47 -0300

    Create database.js

---
+++ database.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("seu link de login", { userNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+  if(err) return console.log("Erro ao conectar no database!\n"+err)
+    console.log("Conectado ao BANCO DE DADOS!")
+})
+
+var Schema = mongoose.Schema
+
+var guilda = new Schema({
+  guild_id: {
+    type: String,
+    default: "-/-",
+    required: true
+  },
+  prefixo: {
+    type: String,
+    default: "seu prefixo default"
+  },
+})
+
+var Guilda = mongoose.model("Guildas", guilda)
+module.exports = {
+  Guilda: Guilda  
+}
